ed97531c939fcfeb5ec1ed2f94b5cf8db1b3b36c
MoeSandar <moesandar213511@gmail.com>
2021-10-10 17:25:05 +0630

    fourth CLI

--- test.php
+++ test.php
@@ -2,8 +2,10 @@
 echo "This is test.php";
 echo "Third line in CLI";
 
+<<<<<<< Updated upstream
  echo "This is test.php";
  echo "Third line in online";
 
  echo "Fourth line from online";
+ echo "Fourth in CLI";
 ?>
